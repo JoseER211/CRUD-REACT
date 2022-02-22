55ea56ba7288dce1fa214def6991eea8f05d94db
JoseER <echavarria11josedavid@gmail.com>
2022-02-22 16:11:14 -0500

    Se creó el componente UserTable para poder visualizar la tabla de usuarios. Se creó el componente EditUserForm para editar los usuarios. Se creó el componente AddUserForm para agregar usuarios. Se creó la función delete en el componente UserTable

---
+++ src/components/AddUserForm.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useForm } from 'react-hook-form'
+
+const AddUserForm = (props) => {
+
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const onSubmit = (data, e) =>{
+       // console.log(data);
+        props.addUser(data)
+        //limpiar campos
+        e.target.reset();
+        
+    
+    
+    } 
+   
+
+    return ( 
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>Name</label>
+      <input type="text" {...register("name", { required: true })} />
+      <span className='text-danger text-small d-block mb-2'>
+      {errors.name && 'Este campo es obligatorio'}
+      </span>
+      <label>Username</label>
+      <input type="text" {...register("Username", { required: true })} />
+      <span className='text-danger text-small d-block mb-2'>
+      {errors.Username && 'Este campo es obligatorio'}
+      </span>
+      <button>Add new user</button>
+    </form>
+     );
+}
+ 
+export default AddUserForm;
